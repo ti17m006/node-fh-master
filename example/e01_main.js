bdfe0478ca76264de96e213808d1565d1ab008a8
ti17m006 <ti17m006@technikum-wien.at>
2019-11-05 17:57:24 +0100

    http example

--- example/e01_main.js
+++ example/e01_main.js
@@ -2,10 +2,8 @@
 const http = require('http');
 const http_methods = require('./e01a').http_methods;
 const PORT = 8000;
-
 const server = http.createServer((request, response) => {
     http_methods(request, response);
 });
-
 server.listen(PORT);
 console.log(`Listening on port ${PORT}`);
