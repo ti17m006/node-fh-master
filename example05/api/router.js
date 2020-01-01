34e27ef10f78449a87e4804bb34222580e6eee5d
ti17m006 <ti17006@technikum-wien.at>
2020-01-01 19:07:25 +0100

    Register

--- example05/api/router.js
+++ example05/api/router.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
+const bcrypt = require('bcrypt');
 const Joi = require('@hapi/joi');
 const schemas = require('./joi_schema');
 const db = require('./db_connection');
@@ -24,7 +25,14 @@ router.post('/manager/register', async (req, res) => {
 		res.status(400).send(`Error ${check.error}`);
 	} else {
 		try {
-			console.log(await Managers(req.body).save());
+			const salt = await bcrypt.genSalt(10);
+			const local_manager = {
+				id: req.body.id,
+				fullname: req.body.fullname,
+				username: req.body.username,
+				password: await bcrypt.hash(req.body.password, salt)
+			};
+			console.log(await Managers(local_manager).save());
 			res.send(`Manager successfully saved.`);
 		} catch (exception) {
 			console.error(`Error save() ${exception}\n`);
@@ -34,7 +42,7 @@ router.post('/manager/register', async (req, res) => {
 });
 
 router.post('/manager/login', async (req, res) => {
-	
+
 });
 
 module.exports = router;
