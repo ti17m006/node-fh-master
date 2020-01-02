dace315d605c539342edbef4fe85b121f6452931
ti17m006 <ti17006@technikum-wien.at>
2020-01-02 16:51:02 +0100

    login joi schema

--- example05/api/joi_schema.js
+++ example05/api/joi_schema.js
@@ -11,3 +11,4 @@ module.exports.JoiWorker = Joi.object({
     username: Joi.string().min(10).max(255).required(),
     password: Joi.string().min(4).max(255).required()
 });
+module.exports.JoiManagerLogin = Joi.object();
