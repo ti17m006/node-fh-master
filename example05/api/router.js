42d8e69639731cc2b40e843fad3da7bf857baec1
ti17m006 <ti17006@technikum-wien.at>
2020-01-04 19:43:37 +0100

    todo: worker

--- example05/api/router.js
+++ example05/api/router.js
@@ -20,6 +20,13 @@ function signManager(payload) {
 	}, 'managerPrivateKey');
 }
 
+function signWorker(payload) {
+	return jwt.sign({
+		id: payload.id,
+		username: payload.username
+	}, 'workerPrivateKey');
+}
+
 router.post('/manager/register', async (req, res) => {
 	const existing = await Managers.findOne({ username: req.body.username });
 	if (existing) {
@@ -73,11 +80,72 @@ router.get('/manager/current', async (req, res) => {
 		res.status(401).send('Empty token');
 	}
 	try {
-		res.send(jwt.verify(req.header('jwt-manager'), 'managerPrivateKey')); 
+		res.send(jwt.verify(req.header('jwt-manager'), 'managerPrivateKey'));
 	} catch (exception) {
 		res.status(400).send('Invalid token');
 	}
 	res.send();
+});
+
+router.post('/worker/register', async (req, res) => {
+	const existing = await Workers.findOne({ username: req.body.username });
+	if (existing) {
+		console.log('User exists');
+		res.send('User exists');
+	}
+	const check = Joi.validate(req.body, schemas.JoiWorker);
+	if (check.error) {
+		console.error(`worker registration error: ${check.error}`);
+		res.status(400).send(`Error ${check.error}`);
+	} else {
+		try {
+			const salt = await bcrypt.genSalt(10);
+			const local_worker = {
+				id: req.body.id,
+				fullname: req.body.fullname,
+				username: req.body.username,
+				password: await bcrypt.hash(req.body.password, salt)
+			};
+			console.log(await Workers(local_worker).save());
+			res.send(`Worker successfully saved.`);
+		} catch (exception) {
+			console.error(`Error save() ${exception}\n`);
+		}
+	}
+});
+
+router.post('/worker/login', async (req, res) => {
+	const payload = await Managers.findOne({ username: req.body.username });
+	const check = Joi.validate(req.body, schemas.JoiManagerLogin);
+	if (check.error) {
+		console.error(`manager error: ${check.error}`);
+		res.status(400).send(`Error ${check.error}`);
+	}
+	if (!payload) {
+		console.log('User does not exist');
+		res.send('User does not exist');
+	}
+	if (await bcrypt.compare(req.body.password, payload.password)) {
+		res.header('jwt-manager', signManager(payload)).send(payload);
+	} else {
+		res.send('Invalid password');
+	}
+})
+
+router.get('/worker/current', async (req, res) => {
+	if (!req.header('jwt-manager')) {
+		res.status(401).send('Empty token');
+	}
+	try {
+		res.send(jwt.verify(req.header('jwt-manager'), 'managerPrivateKey'));
+	} catch (exception) {
+		res.status(400).send('Invalid token');
+	}
+	res.send();
+});
+
+router.put('/worker/location', async (req, res) => {
+
 });
 
 module.exports = router;
