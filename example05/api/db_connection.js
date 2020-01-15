c0a7deb78b393d5ec77113c63cda403e2448f7b6
ti17m006 <ti17m006@technikum-wien.at>
2020-01-15 20:31:30 +0100

    db_connection mongoatlas

--- example05/api/db_connection.js
+++ example05/api/db_connection.js
@@ -2,7 +2,7 @@
 const localhost = '127.0.0.1:27017';
 const database = 'example05';
 const dblink = `mongodb://${localhost}/${database}`;
-const mongoatlas = 'mongodb+srv://master-student:1234@cluster0-eeu87.mongodb.net/test?retryWrites=true&w=majority';
+const mongoatlas = `mongodb+srv://master-student:1234@cluster0-eeu87.mongodb.net/test?retryWrites=true&w=majority/${database}`;
 const Model= require('./db_models');
 const mongoose = require('mongoose');
 class Database {
