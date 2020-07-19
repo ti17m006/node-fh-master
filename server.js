00f3084d77eef24027d6f0440eefdcc13a4ac9b4
ti17m006 <ti17m006@technikum-wien.at>
2020-07-19 19:00:36 +0200

    server graphql

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 
 console.log('app.js');
-const restfulapi = require('./restful-api-v01/app');
-restfulapi;
+
+// const restfulapi = require('./restful-api-v01/app');
+// restfulapi;
+
+const graphql = require('./graphql-v1/app');
+graphql;
