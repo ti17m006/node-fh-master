35bcd6df9700759432fde4690785ccf469c1781e
ti17m006 <ti17m006@technikum-wien.at>
2020-08-09 18:52:39 +0200

    Hash comment added

--- miscellaneous/bcryptHash.js
+++ miscellaneous/bcryptHash.js
@@ -2,6 +2,11 @@ const { genSalt, hash, compare } = require('bcrypt');
 
 // https://www.npmjs.com/package/bcrypt saltRounds = 10 recommended   
 
+/**
+ * 
+ * @param {*} password 
+ * @returns await hash(password, salt);
+ */
 module.exports.hashPasword = async (password) => {
     const rounds = 10;
     let salt = await genSalt(rounds);
