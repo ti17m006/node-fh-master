5f5942a97fbeec32a0ab27a013e79dc898abf72a
ti17m006 <ti17m006@technikum-wien.at>
2020-08-09 19:16:46 +0200

    registe manager, registe worker

--- graphql-v01/mutations/manager_mutation.js
+++ graphql-v01/mutations/manager_mutation.js
@@ -1,46 +1,71 @@
 const { Managers, Workers } = require('../../database/mogodb_connection');
 const { hashPasword } = require('../../miscellaneous/bcryptHash');
-const { validateManager } = require('../../joi_schema/joi_schema');
+const { validateManager, validateWorker } = require('../../joi_schema/joi_schema');
 
 // register
 module.exports.register = async (manager) => {
     try {
-        let local_manager = {
+        let _manager = {
             id: manager.id,
             fullname: manager.fullname,
             username: manager.username,
             password: manager.password
         };
-        const check = validateManager(local_manager);
+        const check = validateManager(_manager);
         if (check.error) {
             throw `Joi validation failed ${check.error}`;
         } else {
-            const result = await Managers.findOne({ username: local_manager.username });
+            const result = await Managers.findOne({ username: _manager.username });
             if (result) {
                 throw 'Manager exists';
             }
-            local_manager.password = await hashPasword(local_manager.password);
-            return Managers.create(local_manager)
-                .then((success) => {
-                    if (success) {
-                        console.log('Successfully saved\n', success);
-                        return true;
-                    } else {
-                        throw 'Not saved';
-                    }
-                })
-                .catch((e) => {
-                    console.error(e);
-                    return false;
-                });
-        };
+            _manager.password = await hashPasword(_manager.password);
+            return Managers.create(_worker)
+                .then((success) => { console.log(`MAnager saved\n${success}`); return true; })
+                .catch((error) => { console.error(error); return false });
+        }
     } catch (exception) {
         console.error(exception);
         return false;
     }
 }
 
+/**
+ * 
+ * @param {"structure: id, fullname, username, password"} worker 
+ * @returns true or false
+ */
+module.exports.newWorker = async (worker, headers) => {
+    try {
+        let _worker = {
+            id: worker.id,
+            fullaname: worker.fullname,
+            username: worker.worker,
+            password: worker.password
+        };
+        let check = validateWorker(_worker);
+        if (check.error) {
+            throw `Joi validation failed ${check.error}`;
+        } else {
+            const worker_db = await Workers.findOne({ username: username });
+            if (worker_db) {
+                throw 'Worker exists.'
+            }
+            _worker.password = await hashPasword(_worker.password);
+            return Workers.create(_worker)
+                .then((success) => { console.log(`Worker saved\n${success}`); return true; })
+                .catch((error) => { console.error(error); return false });
+        }
+    } catch (exception) {
+        console.error(exception);
+        return false;
+    }
+}
 
+/** deleteWorker from a database
+ * @param {"worker's id"} id
+ * @returns true or false
+ */
+module.exports.deleteWorker = (id) => {
 
-//register-worker
-// delete-worker
+}
